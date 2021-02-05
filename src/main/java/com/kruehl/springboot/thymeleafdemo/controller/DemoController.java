c82eca089fcad698c814287d4b50f61f6e3fedae
Ignium12 <kevin.ruehl12@googlemail.com>
2021-02-05 16:14:00 +0100

    Added css file and integrated it into helloworld.html

---
+++ src/main/java/com/kruehl/springboot/thymeleafdemo/controller/DemoController.java
@@ -0,0 +1,18 @@
+package com.kruehl.springboot.thymeleafdemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DemoController {
+    // create mapping for "/hello"
+
+    @GetMapping("/hello")
+    public String sayHello(Model model){
+
+        model.addAttribute("date", new java.util.Date());
+
+        return "helloworld";
+    }
+}
